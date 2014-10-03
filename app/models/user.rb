acd537dd7115edcc57b4f3d67c48129a77db1a2b
Alexander Taylor <a@alexandertaylor.org>
2014-10-03 22:23:24 +0100

    Set health/mana on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_create :default_status
+
+
+  def default_status
+  	self.health = 100
+  	self.mana = 100
+  end
 
 end
