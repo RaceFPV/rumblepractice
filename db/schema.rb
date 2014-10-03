9d627eab67efc4b2afaddf5521d7f4a07c32760c
Alexander Taylor <a@alexandertaylor.org>
2014-10-03 19:03:23 +0100

    Add character model that belongs_to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003171636) do
+ActiveRecord::Schema.define(version: 20141003175738) do
+
+  create_table "characters", force: true do |t|
+    t.string   "name"
+    t.integer  "health"
+    t.integer  "mana"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "characters", ["user_id"], name: "index_characters_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
